e4142c860f102cea5f8281a706698abc53aa9617
WombatFromHell <WombatFromHell@users.noreply.github.com>
2025-02-13 12:53:41 -0700

    Expose a devShell with rust-toolchain.toml support in flake.nix

--- flake.nix
+++ flake.nix
@@ -21,30 +21,65 @@
     nixpkgs,
     flake-utils,
     naersk,
-  }: let
-    overlay = final: prev: {
-      veridian-controller = self.packages.${prev.system}.veridian-controller;
-    };
-  in
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-      naersk' = pkgs.callPackage naersk {};
-    in {
-      packages = {
-        veridian-controller = naersk'.buildPackage {
-          pname = "veridian-controller";
-          version = "0.2.6";
-          src = ./.;
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        naersk' = pkgs.callPackage naersk {};
+        overrides = pkgs.formats.toml.parse (builtins.readFile ./rust-toolchain.toml);
+        rustcVersion = overrides.toolchain.channel;
+        llvmPackagesLatest = pkgs.llvmPackages_latest;
+        libPath = with pkgs;
+          pkgs.lib.makeLibraryPath [
+            # load external libraries that you need in your rust project here
+          ];
+        bindgenExtraClangArgs =
+          # Includes normal include path
+          (builtins.map (a: "-I${a}/include") [
+            # add dev libraries here (e.g. pkgs.libvmi.dev)
+          ])
+          # Includes with special directory paths
+          ++ [
+            "-I${llvmPackagesLatest.libclang.lib}/lib/clang/${llvmPackagesLatest.libclang.version}/include"
+          ];
+      in {
+        packages = {
+          veridian-controller = naersk'.buildPackage {
+            pname = "veridian-controller";
+            version = "0.2.7";
+            src = ./.;
+          };
+          default = self.packages.${system}.veridian-controller;
         };
-        default = self.packages.${system}.veridian-controller;
-      };
 
-      apps.default = flake-utils.lib.mkApp {
-        drv = self.packages.${system}.default;
+        apps.default = flake-utils.lib.mkApp {
+          drv = self.packages.${system}.default;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            clang
+            llvmPackagesLatest.bintools
+            rustup
+          ];
+          RUSTC_VERSION = rustcVersion;
+          LIBCLANG_PATH = pkgs.lib.makeLibraryPath [llvmPackagesLatest.libclang.lib];
+          shellHook = ''
+            export PATH=$PATH:$CARGO_HOME/bin
+            export PATH=$PATH:$RUSTUP_HOME/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
+          ''; # shellHook is optional but kept for path extension as in original shell.nix
+          RUSTFLAGS = builtins.map (a: "-L ${a}/lib") [
+            # add libraries here (e.g. pkgs.libvmi)
+          ];
+          LD_LIBRARY_PATH = libPath;
+          BINDGEN_EXTRA_CLANG_ARGS = bindgenExtraClangArgs;
+        };
+      }
+    )
+    // rec {
+      overlays.default = final: prev: {
+        inherit (self.packages.${prev.system}) veridian-controller;
       };
-    })
-    // {
-      overlays.default = overlay;
 
       nixosModules.default = {
         config,
@@ -53,7 +88,7 @@
         ...
       }: {
         config = {
-          nixpkgs.overlays = [overlay];
+          nixpkgs.overlays = [overlays.default];
         };
       };
     };
