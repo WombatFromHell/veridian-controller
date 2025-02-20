9b89f1a6c9f7c86c094c7ecbae29040295e20b34
WombatFromHell <WombatFromHell@users.noreply.github.com>
2025-02-20 13:44:20 -0700

    Move shell.nix into flake.nix and expose a devShells.default for .envrc to use

--- flake.nix
+++ flake.nix
@@ -31,6 +31,24 @@
           } ''
             toml2json ${./Cargo.toml} | jq -r '.package.version' > $out
           ''));
+        #
+        overrides = builtins.fromTOML (builtins.readFile ./rust-toolchain.toml);
+        rustcVersion = overrides.toolchain.channel;
+        llvmPackagesLatest = pkgs.llvmPackages_latest;
+        libPath = with pkgs;
+          pkgs.lib.makeLibraryPath [
+            # load external libraries that you need in your rust project here
+          ];
+        bindgenExtraClangArgs =
+          # Includes normal include path
+          (builtins.map (a: "-I${a}/include") [
+            # add dev libraries here (e.g. pkgs.libvmi.dev)
+            pkgs.glibc.dev
+          ])
+          # Includes with special directory paths
+          ++ [
+            "-I${llvmPackagesLatest.libclang.lib}/lib/clang/${llvmPackagesLatest.libclang.version}/include"
+          ];
       in {
         packages = {
           veridian-controller = naersk'.buildPackage {
@@ -43,6 +61,25 @@
 
         apps.default = flake-utils.lib.mkApp {
           drv = self.packages.${system}.default;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            clang
+            llvmPackagesLatest.bintools
+            rustup
+          ];
+          RUSTC_VERSION = rustcVersion;
+          LIBCLANG_PATH = pkgs.lib.makeLibraryPath [llvmPackagesLatest.libclang.lib];
+          shellHook = ''
+            export PATH=$PATH:$CARGO_HOME/bin
+            export PATH=$PATH:$RUSTUP_HOME/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
+          '';
+          RUSTFLAGS = builtins.map (a: "-L ${a}/lib") [
+            # add libraries here (e.g. pkgs.libvmi)
+          ];
+          LD_LIBRARY_PATH = libPath;
+          BINDGEN_EXTRA_CLANG_ARGS = bindgenExtraClangArgs;
         };
       }
     )
